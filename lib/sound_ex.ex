26da41f25b60c976894a459b8d01770d36c27002
vicvans20 <vicvans20@gmail.com>
2017-05-08 13:16:30 -0600

    First commit

---
+++ lib/sound_ex.ex
@@ -0,0 +1,18 @@
+defmodule SoundEx do
+  @moduledoc """
+  Documentation for SoundEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SoundEx.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sound_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule SoundExTest do
+  use ExUnit.Case
+  doctest SoundEx
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
