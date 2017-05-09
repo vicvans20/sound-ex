7de3cac952a17a8eb62f7197ba7fb9fc6693c304
vicvans20 <vicvans20@gmail.com>
2017-05-08 18:07:57 -0600

    Add button setup to stop the loop.

--- lib/sound_ex.ex
+++ lib/sound_ex.ex
@@ -9,6 +9,9 @@ defmodule SoundEx do
   # @melody2 [
   #   {:G6,12}, {:E7,12}, {:G7,12}
   # ]
+  @piezo 9
+  @led 8
+  @button 7
 
   @initial_state %{}
 
@@ -20,11 +23,21 @@ defmodule SoundEx do
     GenServer.start_link(__MODULE__, {device_port, opts}, name: __MODULE__)
   end
 
+  def get_button_value(pin) do
+    GenServer.call(__MODULE__, {:get_button_value, pin})
+  end
+
   # Server
   
   def init({device_port, opts }) do
     Archytax.start_link(device_port, opts)
     {:ok, @initial_state}
+  end
+
+  def handle_call({:get_button_value, pin}, _from, state) do
+    {:ok, pins} = Archytax.get_pins
+    digital_value = pins[pin][:value] || 0
+    {:reply, {:ok, digital_value}, state}
   end
 
 
@@ -33,8 +46,14 @@ defmodule SoundEx do
   ######################
   # Get board ready setup signal to setup the circuit.
   def handle_info({:archytax, {:ready, _anything}}, state ) do
-    Archytax.set_pin_mode(9, 3)
-    Archytax.set_pin_mode(8, 1)
+    delay(2000)
+    Archytax.set_pin_mode(@piezo, 3) 
+    Archytax.set_pin_mode(@led, 1)
+
+    Archytax.set_pin_mode(@button, 0) # Input mode
+    Archytax.report_digital_port(@button ,1) # Report digital port value
+
+    # Start
     beep(50)
     beep(50)
     beep(50)
@@ -51,28 +70,40 @@ defmodule SoundEx do
 
   # Circuit Functionality
   defp loop do
-    # Enum.each(@melody1, &play_note/1)
-    # delay(2000)
-    # Enum.each(@melody2, &play_note/1)
-    # delay(2000)
+    {:ok, button_value} = SoundEx.get_button_value(@button)
+    exec_loop(button_value)
+  end
+
+  # Pressed
+  def exec_loop(1) do
     beep(200)
     loop()
   end
 
+  # Not Pressed
+  def exec_loop(0) do
+    :ok
+  end
+
   def play_note({note, tempo}) do
-    Archytax.Tone.play(9, note, tempo)
-    Archytax.set_digital_pin(8,1)
+    Archytax.Tone.play(@piezo, note, tempo)
+    Archytax.set_digital_pin(@led,1)
     delay(100)
-    Archytax.set_digital_pin(8,0)
+    Archytax.set_digital_pin(@led,0)
     delay(1000)
   end
 
   def beep(dms) do
-    Archytax.analog_write(9, 20)
-    Archytax.set_digital_pin(8,1)
+    Archytax.analog_write(@piezo, 20)
+    Archytax.set_digital_pin(@led,1)
     delay(dms)
-    Archytax.analog_write(9,0)
-    Archytax.set_digital_pin(8,0)
+    Archytax.analog_write(@piezo,0)
+    Archytax.set_digital_pin(@led,0)
     delay(dms)
   end
+
 end
+    # Enum.each(@melody1, &play_note/1)
+    # delay(2000)
+    # Enum.each(@melody2, &play_note/1)
+    # delay(2000)
